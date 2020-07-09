7e4e4093421ab80aac31a2be96d4a559ea03b2f8
haiduk <hwlee91@gmail.com>
2020-07-09 03:46:42 -0400

    basic account

---
+++ bank_account.rb
@@ -0,0 +1,29 @@
+class Account
+  attr_reader :name, :balance
+    def initialize(name, balance=100)
+      @name = name
+      @balance = balance
+    end
+
+  def display_balance(pin_number)
+    pin_number == pin ? "Balance: $#{@balance}." : pin_error
+  end
+
+  def withdraw(pin_number, amount)
+    pin_number == pin ? "Withdrew #{amount}. New Balance: 
+    $#{@balance}." : pin_error
+  end
+
+  private
+    def pin
+      @pin = 1234
+    end
+  
+    def pin_error
+      "Access denied: incorrect PIN."
+    end
+  end
+end
+
+checking_account = Account.new("Hank", 3.50)
+}
